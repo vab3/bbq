3da6ffb7bbc79f0b6e8fc6682cc0baf4eb8f69ed
Vic Boudolf <vic@boudolf.com>
2021-02-06 16:40:54 -0500

    add locations by state for sc

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,4 +8,25 @@ class LocationsController < ApplicationController
                          .where(location_type: 0)
                          .order('vendors.name')
   end
+
+
+  def states
+  end
+
+  def by_state
+    redirect_to restaurants_url unless params[:state].present?
+    redirect_to restaurants_url unless state_is_valid(params[:state]) 
+    @locations = Location.joins(:vendor)
+                         .where(state: params[:state], location_type: 0)
+                         .order('vendors.name')
+    render "index"
+  end
+
+  def state_is_valid state
+    valid_us_states.include? state.upcase
+  end
+
+  def valid_us_states
+    ["SC", "NC"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :locations
   get '/restaurants', to: 'locations#restaurants'
   get '/states', to: 'locations#states'
+  get '/locations/by_state/:state', to: 'locations#by_state', as: 'locations_by_state'
   root to: "vendors#index"
 end
